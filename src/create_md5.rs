d8f6e93800cd9891298e02b9230e567c5e872ab4
陶辰 <chen.tao@ifchange.com>
2020-01-22 15:56:16 +0800

    update

--- src/create_md5.rs
+++ src/create_md5.rs
@@ -6,7 +6,7 @@ mod tests {
         let opt = create_md5::create_md5::Opt{
             input: PathBuf::from("src/main.rs"),
             output: "l".to_string(),
-            t: "str".to_string(),
+            t: "file".to_string(),
         };
         let res = create_md5::create_md5::create_md5(opt).unwrap();
         return res
@@ -14,18 +14,18 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(create_md5_test(), "639fbc4ef05b315af92b4d836c31b023".to_string());
+        assert_eq!(create_md5_test(), "b29827ecc0ced7dc011014868259a780".to_string());
     }
 }
 
-
-
 pub mod create_md5 {
     use structopt::StructOpt;
-    use std::path::PathBuf;
-    use failure::ResultExt;
     use exitfailure::ExitFailure;
-    use quicli::prelude::read_file;
+    use std::path::{Path, PathBuf};
+    use std::io::{BufReader, Read};
+    use std::fs::File;
+    use failure::{Error, ResultExt, ensure};
+
     #[derive(StructOpt, Debug)]
     pub struct Opt {
         /// Input string or file path
@@ -59,6 +59,22 @@ pub mod create_md5 {
             val = val.to_uppercase()
         }
         Ok(val)
+    }
+
+    pub fn read_file<P: AsRef<Path>>(path: P) -> Result<Vec<u8>, Error> {
+        let path = path.as_ref();
+        ensure!(
+        path.exists() && path.is_file(),
+        "Path {:?} is not a file!",
+        path
+    );
+        let file = File::open(path).with_context(|_| format!("Could not open file {:?}", path))?;
+        let mut file = BufReader::new(file);
+        let mut result = Vec::new();
+        file.read_to_end(&mut result)
+            .with_context(|_| format!("Could not read file {:?}", path))?;
+
+        Ok(result)
     }
 }
 
